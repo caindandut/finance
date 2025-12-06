49b54c44282f9248d8c154b4574d853d85095a4a
ledangkhanhdut <khanhdangabc2@gmail.com>
2025-12-06 20:41:31 +0700

    feat(be): add security and logging middlewares

--- server/server.js
+++ server/server.js
@@ -5,6 +5,14 @@ const connectDB = require('./config/db.js');
 connectDB();
 const app = express();
 
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
