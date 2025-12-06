dd3c28c077d2c7f1ec8be448bccaddbda8f99092
ledangkhanhdut <khanhdangabc2@gmail.com>
2025-12-06 20:39:33 +0700

    feat(be): setup basic express server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db.js');
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
