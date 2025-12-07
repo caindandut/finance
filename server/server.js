c38d1684b8cb91aeb794a4b63207530a1fad8169
ledangkhanhdut <khanhdangabc2@gmail.com>
2025-12-07 19:46:04 +0700

    refactor(be): integrate error middleware into server

--- server/server.js
+++ server/server.js
@@ -9,11 +9,21 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const errorHandler = require('./middlewares/errorMiddleware');
+
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 
 app.use(express.json());
+
+app.get('/error', (req, res, next) => {
+    const error = new Error('Test Error Middleware');
+    error.statusCode = 400;
+    next(error);
+});
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
