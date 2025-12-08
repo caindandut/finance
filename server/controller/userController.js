fc8030bb782c2f929684195346a1b2f89b7a808c
ledangkhanhdut <khanhdangabc2@gmail.com>
2025-12-08 15:02:49 +0700

    feat(be): implement get user profile logic

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -67,7 +67,15 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-   res.send('Profile Route');
+  const getUserProfile = asyncHandler(async (req, res) => {
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  
+  res.status(200).json(user);
+});
 });
 
 module.exports = {
